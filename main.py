21d65622e7038703590f80604729794a52c0e7d6
Justin Bane <justbane@gmail.com>
2024-05-02 01:11:12 +0000

    Updated to just show records

--- main.py
+++ main.py
@@ -10,39 +10,41 @@ from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 from llama_index.llms.openai import OpenAI
 from pdf import pass_car_engine
+import matplotlib
 
 car_data = os.path.join("data","car_prices.csv")
 car_df = pd.read_csv(car_data)
 
-car_query_eng = PandasQueryEngine(df=car_df, verbose=True, instruction_str=instruction_str)
-car_query_eng.update_prompts({
-    'pandas_prompt': new_prompt
-})
+car_query_eng = PandasQueryEngine(df=car_df)
+# car_query_eng = PandasQueryEngine(df=car_df, verbose=False, instruction_str=instruction_str, synthesize_response=True)
+# car_query_eng.update_prompts({
+    # 'pandas_prompt': new_prompt
+# })
 
-tools = [
-    note_engine,
-    QueryEngineTool(
-        query_engine=car_query_eng, 
-        metadata=ToolMetadata(
-            name="car_sales_data",
-            description="provides information on car sales data"
-        )
-    ),
-    QueryEngineTool(
-        query_engine=pass_car_engine, 
-        metadata=ToolMetadata(
-            name="passenger_car_data",
-            description="provides detailed information on passenger cars in the United States"
-        )
-    )
-]
+# tools = [
+#     note_engine,
+#     QueryEngineTool(
+#         query_engine=car_query_eng, 
+#         metadata=ToolMetadata(
+#             name="car_sales_data",
+#             description="provides information on car sales data"
+#         )
+#     ),
+#     QueryEngineTool(
+#         query_engine=pass_car_engine, 
+#         metadata=ToolMetadata(
+#             name="passenger_car_data",
+#             description="provides detailed information on passenger cars in the United States"
+#         )
+#     )
+# ]
 
-llm = OpenAI(model="gpt-3.5-turbo-0125")
-agent = ReActAgent.from_tools(tools, llm=llm, verbose=False, context=context)
+# llm = OpenAI(model="gpt-3.5-turbo-0125")
+# agent = ReActAgent.from_tools(tools, llm=llm, verbose=False, context=context)
 
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
     try:
-        result = agent.query(prompt)
+        result = car_query_eng.query(prompt)
     except ValueError:
         result = print("Oops! There was error with your request. Try again!")
     
